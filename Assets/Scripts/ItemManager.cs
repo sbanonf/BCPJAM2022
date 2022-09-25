d76799e7ad03f86893adae6013030d0994328df8
JAVHM <javanhmalpro@gmail.com>
2022-09-25 13:42:59 -0500

    minijuego canvas

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Linq;
+using TreeEditor;
 
 public class ItemManager : MonoBehaviour
 {
     public static ItemManager itemManagerInstance;
+    public ScriptableIns[] scriptableIns;
 
     private void Awake()
     {
@@ -20,10 +22,28 @@ public class ItemManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Update()
+    {
+        SetearTexto();
+    }
+
     public GameObject floatingText;
     public void showValue(InsumoData insumoData, Transform transformCanasta)
     {
         var t = Instantiate(floatingText, transformCanasta.position, Quaternion.identity);
         t.GetComponent<TextMeshPro>().text = "-/s." + insumoData.insumo.costo;
     }
+
+    public TextMeshProUGUI amounts;
+
+    public void SetearTexto()
+    {
+        amounts = GameObject.FindGameObjectWithTag("InsumosText").GetComponent<TextMeshProUGUI>();
+        amounts.text = "";
+        for (int i = 0; i < scriptableIns.Length; i++)
+        {
+            amounts.text = amounts.text + "   " + scriptableIns[i].nombre + "|" + scriptableIns[i].cantidad;
+        }
+    }
+
 }
